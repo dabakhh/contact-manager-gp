c6ca713e91eefae7a636158e7b0027c7e5a28fc2
Abdou Aziz DIOP <dabakhabdouaziz129@gmail.com>
2026-03-20 22:31:21 +0000

    Trying to display contacts

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,27 +1,28 @@
 let contacts = []; // Tableau pour stocker les contacts
 const contactForm = document.getElementById('contactForm');
 const contactsList = document.getElementById('contactsList')
-const ul = document.createElement('ul')
-let li = document.createElement('li')
-function displayContacts(contactArray) {
+const monUL = document.createElement('ul')
+// function displayContacts(contactArray) {
 
-    contactArray.forEach(element => { // Display contacts one by one
+//     contactArray.forEach(element => { // Display contacts one by one
+    
+//     let li = document.createElement('li')
+    
+//     li= element;
+//     monUL.appendChild(li);
 
-    li= element;
-    // ul.appendChild(li);
-
-    });
-}
+//     });
+// }
 
 contactForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire
     
     // Récupérer les valeurs des champs du formulaire
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const phone = +document.getElementById('phone').value;
-    const selectElement = document.getElementById('selectCategory').value
+    // const firstName = document.getElementById('firstName').value;
+    // const lastName = document.getElementById('lastName').value;
+    // const email = document.getElementById('email').value;
+    // const phone = +document.getElementById('phone').value;
+    // const selectElement = document.getElementById('selectCategory').value
     const statut = document.getElementById('favorite');
     const verified = statut.checked;
     
@@ -29,21 +30,23 @@ contactForm.addEventListener('submit', function(event) {
     
     // Créer un objet contact
     const contact = {
-        firstName,
-        lastName,
-        email,
-        phone,
-        selectElement,
+        // firstName,
+        // lastName,
+        // email,
+        // phone,
+        // selectElement,
         verified,
     };
     
     
     // // Ajouter le contact au tableau
     contacts.push(contact);
-    
+    console.log(contacts)
+
+
     // Afficher les contacts (tu devras créer cette fonction)
-    displayContacts(contacts);
-    contactsList.appendChild(ul);
+    // displayContacts(contacts);
+    // contactsList.appendChild(ul);
     
     // // Réinitialiser le formulaire
     // contactForm.reset();
